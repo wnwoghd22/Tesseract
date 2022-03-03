680e501c74520f88379fe59453d18a39b8e67055
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-03-03 23:50:54 +0900

    ready to make basic components

---
+++ Assets/Mirror/LaserBeam.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // TODO: watch video https://www.youtube.com/watch?v=pNE3rfMGEAw and implement mirror
+}
