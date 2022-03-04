acff6d7fadb7070432f35da9071d5604daea37dc
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-03-04 23:26:46 +0900

    implemented laser

--- Assets/Mirror/LaserBeam.cs
+++ Assets/Mirror/LaserBeam.cs
@@ -2,19 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserBeam : MonoBehaviour
+public class LaserBeam
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector3 pos, dir;
+
+    GameObject laserObj;
+    LineRenderer laser;
+
+    List<Vector3> laserIndices = new List<Vector3>();
+
+    public LaserBeam(Vector3 pos, Vector3 dir, Material material)
     {
-        
+        this.laser = new LineRenderer();
+        this.laserObj = new GameObject();
+        this.laserObj.name = "Laser Beam";
+        this.pos = pos;
+        this.dir = dir;
+
+        this.laser = this.laserObj.AddComponent(typeof(LineRenderer)) as LineRenderer;
+        this.laser.startWidth = 0.5f;
+        this.laser.endWidth = 0.3f;
+        this.laser.material = material;
+        this.laser.startColor = Color.yellow;
+        this.laser.endColor = Color.yellow;
+
+        CastRay(pos, dir, laser);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CastRay(Vector3 pos, Vector3 dir, LineRenderer laser)
     {
-        
+        laserIndices.Add(pos);
+
+        Ray2D ray = new Ray2D(pos, dir);
+        RaycastHit2D hit = Physics2D.Raycast(pos, dir);
+
+        if (hit)
+        {
+            laserIndices.Add(hit.point);
+            UpdateLaser();
+        }
+        else
+        {
+            laserIndices.Add(ray.GetPoint(30));
+            UpdateLaser();
+        }
     }
 
-    // TODO: watch video https://www.youtube.com/watch?v=pNE3rfMGEAw and implement mirror
+    void UpdateLaser()
+    {
+        int count = 0;
+        laser.positionCount = laserIndices.Count;
+
+        foreach (Vector3 idx in laserIndices)
+        {
+            laser.SetPosition(count, idx);
+            ++count;
+        }
+    }
 }
--- Assets/Mirror/Mirror.cs
+++ Assets/Mirror/Mirror.cs
@@ -1,22 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Mirror : MonoBehaviour
 {
     private bool isLit;
 
+    [SerializeField]
+    Material material;
+    LaserBeam beam;
+
+    Vector3 touchPos;
+
     // Start is called before the first frame update
     void Start()
     {
         isLit = false;
+        touchPos = gameObject.transform.right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isLit)
+        {
+            Destroy(GameObject.Find("Laser Beam"));
+            //beam = new LaserBeam(gameObject.transform.position, touchPos, material);
+
+            beam = new LaserBeam(gameObject.transform.position, gameObject.transform.right, material);
+        }
     }
 
+    private void OnMouseDown()
+    {
+        Debug.Log("on click");
+        isLit = true;
+    }
 
 }
